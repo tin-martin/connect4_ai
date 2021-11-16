0908d404ac8f40b92e17805f3498b8f94bf41fcb
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-11-16 11:05:11 -0500

    Update c4.py

--- c4/c4.py
+++ c4/c4.py
@@ -1,19 +1,20 @@
 from Player import Player
 from gameBoard import gameBoard
 if __name__ == '__main__':	
-	gameBoard = gameBoard(7,5)
+	gameBoard = gameBoard(7,6)
 	player1 = Player("X",gameBoard)
 	player2 = Player("O",gameBoard)	
 	i=0
 	while(True):
 		if(i % 2 == 0):
-			player1.move(int(input("Enter move:")))
+			gameBoard.board = gameBoard.move("O", int(input("Enter move:")))
 		else:
-			player2.move(int(input("Enter move:")))
+			gameBoard.board = gameBoard.move("X", int(input("Enter move:")))
 
+		
 		gameBoard.printBoard()
-		if(gameBoard.check("X") == "win"):
-			print("And the winner is... X")
-		elif(gameBoard.check("O") == "win"):
-			print("And the winner is... O")
+		isFinished, winner = gameBoard.isTerminal()
+
+		if(isFinished):
+			print("The winner is... ", winner)
 		i += 1
