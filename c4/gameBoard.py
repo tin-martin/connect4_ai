5bc72c2abe5b394bbca80dc0274e9130a24c3cc6
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-11-16 11:05:27 -0500

    Update gameBoard.py

--- c4/gameBoard.py
+++ c4/gameBoard.py
@@ -9,7 +9,7 @@ class gameBoard:
 	def printBoard(self):
 		for i in range(self.rows+2):
 			print("-",end="")
-		print()
+		print("")
 		for i in range(len(self.board[0])):
 			print("|",end="")
 			for j in range(len(self.board)):
@@ -17,9 +17,20 @@ class gameBoard:
 			print("|")
 		for i in range(self.rows+2):
 			print("-",end="")
-		print()
+		print("")
 	def setBoard(self, new_board):
 		self.board = new_board
+#_________________
+	def move(self, symbol,row):
+		board = self.getBoard()
+		for i in range(len(board[row])):
+			if (board[row][i] != " "):
+				board[row][i-1] = symbol
+				break
+			if(i == len(board[row])-1):
+				board[row][i] = symbol			
+		return board
+#__________________
 	def check_straight(self,board,x,y):
 		symbol = board[x][y]
 		if(board[x][y] == symbol and board[x][y+1] == symbol and board[x][y+2] == symbol and board[x][y+3] == symbol):
@@ -30,7 +41,7 @@ class gameBoard:
 		if(board[x][y] == symbol and board[x+1][y+1] == symbol and board[x+2][y+2] == symbol and board[x+3][y+3] == symbol):
 			return True
 		return False
-	def check(self,symbol):
+	def isTerminal(self):
 		board_tbc = np.array(self.board)
 		for z in range(4):
 			for x in range(len(board_tbc.tolist())):
@@ -38,13 +49,11 @@ class gameBoard:
 					if board_tbc[x][y] != ' ':	
 						try:
 							if(self.check_diagonal(board_tbc.tolist(),x,y) or self.check_straight(board_tbc.tolist(),x,y)):
-								if(board_tbc[x][y] == symbol):
-									return "win"
-								else:
-									return "loss"
+								return True, board_tbc[x][y]
 						except:
 							pass
 			board_tbc = np.rot90(board_tbc,axes=(1,0))	
-		return "incomplete"
+		return False, ""
+
 
 
