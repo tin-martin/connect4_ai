f7ef26a25291bf0fbca1c4734d1d2116f860ba9e
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-12-10 17:32:26 -0500

    Update mcts.py

--- mcts.py
+++ mcts.py
@@ -11,7 +11,9 @@ class Node:
 		self.childs = []
 		self.legal_actions = self.get_legal_actions()
 
-		self.untried_actions = deepcopy(self.legal_actions)
+		self.untried_actions = deepcopy(self.get_legal_actions())
+		
+
 		#self.untried_actions = self.legal_actions()
 
 
@@ -30,14 +32,15 @@ class Node:
 
 	def calculate_ucb1(self):
 		if(self.parent == None):
-			self.parent_simulations = 0
+			self.parent_simulations = 1
 		else:
 			self.parent_simulations = self.parent.simulations
 		try:
 			ucb1 = (self.win/self.simulations) + self.c*(math.log(self.parent_simulations)/self.simulations)
-			self.ucb1 = ucb1
+			self.ucb1 = ucb1 ###############                    ucb1
 			return ucb1
 		except:
+			self.ucb1 = 0
 			return 0
 
 	def get_legal_actions(self):  
@@ -112,20 +115,49 @@ class Node:
 
 class mctsAgent:
 	def __init__(self,state):
-
+		self.state
 		self.root_state = deepcopy(state)
 		#does a move
 		self.root_node = Node(self.root_state)
 
 	def print_childs(self,node): 
+	
 		for child in node.childs:
-			print(child.simulations,end="\t")
-		print("")
-		for child in node.childs:
-			self.print_childs(child)
+			return str(child.win)+"/"+str(child.simulations)+"\t"
 
-	def print_tree(self):
-		self.print_childs(self.root_node)
+		
+	
+
+	def print_depth(self,node,depth):
+		if(depth == 1):
+			return self.print_childs(node)
+		else:
+			
+			tempStr = ""
+
+			for i in range(depth):
+				for child in node.childs:
+					a = self.print_depth(child,depth-1)
+					if (a != None):
+						tempStr += a 
+			return tempStr
+		
+			
+		
+
+
+
+
+
+	def print_tree(self,root_node):	 
+		print(self.print_childs(root_node))
+		depth = 2
+
+		
+		for i in range(1000):
+			print(i,"\t",self.print_depth(root_node,i))
+
+		
 
 	def check_straight(self,board,x,y):
 			symbol = board[x][y]
@@ -169,12 +201,15 @@ class mctsAgent:
 				if(node.childs[i].ucb1 > _max):
 					_max = node.childs[i].ucb1 
 			child = node.childs[i]
-			self.select(child)
+			
+			return self.select(child)
 
 		else:
 			isEnd, winner = node.isTerminal()
 			if(isEnd):
-				update(node,winner)
+				self.update(node,winner)
+
+				
 			else:
 				new_state, action = self.expand(node)
 				if(node.symbol == "X"):
@@ -183,25 +218,27 @@ class mctsAgent:
 					new_symbol = "X"
 
 				child = Node(new_state,node,new_symbol)
-
-				node.childs.append(child)
-				return child;
 				
+				node.childs.append(child)
+				#print("id:",id(node.childs))
+
+				return child
+						
 				
 
 
 	def expand(self,node):
 		temp_action = random.choice(node.untried_actions)
 		node.untried_actions.remove(temp_action)
-
 		return node.move(temp_action), temp_action
 			
 	def simulate(self,node):
 		#from node, get all possible randoms and select one, repeat
+
 		temp_node = deepcopy(node)
 
 	#	def __init__(self,state: tuple = None,parent: object = None, symbol: str = None):
-		while(True):
+		while True:
 			new_state = temp_node.move(random.choice(temp_node.get_legal_actions()))
 			if(temp_node.symbol == "X"):
 				new_symbol = "O"
@@ -212,42 +249,73 @@ class mctsAgent:
 			isEnd, winner = self.isTerminal(child.state)
 			if not (isEnd):
 				temp_node = child
-				continue
 			else:
 				child.printBoard()
 				self.backprop(node,winner)
 				break
 
+
 	def update(self,node,winner):
 		if(node.symbol == winner):
 			node.win += 1
 		node.simulations += 1
+		print("ucb1: ",node.ucb1)
+
+
 		node.calculate_ucb1()
-
-
+		print("ucb1: ",node.ucb1)
 	def backprop(self,node,winner):
-
 		temp_node = node
 		while(temp_node.parent != None):
 			self.update(temp_node,winner)
 			temp_node = temp_node.parent
-
-
-		
 
 	def runSearch(self,state,timeout):
 		pass
 	def bestPlay(self,state):
 		pass
 
+	def move(self, row):
+		board = deepcopy(self.state)
+		symbol = self.symbol 
+
+		if(board[row][0] != " "):
+			raise ValueError('A very specific bad thing happened.')
+
+		for i in range(len(board[row])):
+			if (board[row][i] != " "):
+				board[row][i-1] = symbol
+				break
+			if(i == len(board[row])-1):
+				board[row][i] = symbol			
+		return board
+	def printBoard(self,state):
+		for i in range(len(state)+2):
+			print("-",end="")
+		print("")
+		for i in range(len(state[0])):
+			print("|",end="")
+			for j in range(len(state)):
+				print(state[j][i], end="")
+			print("|")
+		for i in range(self.rows+2):
+			print("-",end="")
+		print("")
+
 if __name__ == "__main__":
 	state = [[' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-	for i in range(3):
-		agent = mctsAgent(state)
+	agent = mctsAgent(state)
+	
+	for i in range(1000): 
 		child_node = agent.select(agent.root_node)
-		agent.simulate(child_node)
+		if not(child_node is None):
+			agent.simulate(child_node)
+
+ #	for i in range(10):
+	#	state = agent.move(agent.bestPlay(state))
+	#	agent.printBoard(state)
+	
 
 
-
-	agent.print_tree()
+	#agent.print_tree(agent.root_node)
 
