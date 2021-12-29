4cd0710a70c6cf8616a52092ca76cdb4e7946fb5
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-12-29 18:19:04 -0500

    Update mcts.py

--- mcts.py
+++ mcts.py
@@ -9,6 +9,12 @@ from c4 import gameBoard
 from copy import deepcopy
 import math
 
+
+import time
+start_time = time.time()
+
+
+
 class Node:
     def __init__(self, gameBoard, parent: object = None, symbol: str = "X"):
         self.gb = gameBoard
@@ -20,46 +26,33 @@ class Node:
         self.parent = parent
         self.childs = []
 
-        self.legal_actions = self.get_legal_actions()
-        self.untried_actions = self.get_legal_actions()
+        self.legal_actions = self.gb.get_legal_actions()
+        self.untried_actions = self.gb.get_legal_actions()
 
         self.wins = 0
         self.visits = 0
-        if(self.parent == None):
-            self.visits  = 1
-        else:
-            self.ucb1 = self.calculate_ucb1()
+        self.ucb1 = self.calculate_ucb1()
 
         self.symbol = symbol
 
         self.action = None
-    
-    def get_legal_actions(self):
-        legal_actions = []
-        for i in range(self.rows):
-            if(self.state[i][0] == " "):
-                legal_actions.append(i)
-        self.legal_actions = legal_actions
-        return legal_actions
+
 
     def calculate_ucb1(self):
-        if self.visits == 0:
+        if(self.visits == 0):
             return 0
         ####################################
-        if self.parent.parent == None:
+        if(self.parent.parent == None):
             self.parent.visits = 0
             for child in self.parent.childs:
                 self.parent.visits += child.visits
         ########################################
-
+        #print(self.wins/self.visits + np.sqrt(2)*np.sqrt(np.log(self.parent.visits)/self.visits))
         return self.wins/self.visits + np.sqrt(2)*np.sqrt(np.log(self.parent.visits)/self.visits)
 
     def select(self):
-        foo = lambda x:  x.calculate_ucb1()
+        foo = lambda x:  x.ucb1
         return sorted(self.childs,key=foo)[-1]
-    
-
-
 
     def expand(self):
         temp_action = random.choice(self.untried_actions)
@@ -86,13 +79,13 @@ class Node:
 
         gb = deepcopy(self.gb)
         isFinished, winner = gb.isTerminal()
-        symbol = self.symbol
+        symbol = deepcopy(self.symbol)
         while not(isFinished):
             if(symbol == "X"):
                 symbol = "O"
             else:
                 symbol = "X"
-
+                
             gb.board = gb.move(symbol,random.choice(gb.get_legal_actions()))
 
             isFinished, winner = gb.isTerminal()
@@ -101,7 +94,7 @@ class Node:
         return winner
 
     def update(self, winner):
-        if(self.symbol == winner):
+        if(self.symbol == winner or "TIE" == winner):
             self.wins += 1
         self.visits += 1
     
@@ -109,7 +102,7 @@ class Node:
         node = self
         while node.parent is not None:
             node.update(winner)
-            node.calculate_ucb1()
+            node.ucb1 = node.calculate_ucb1()
             node = node.parent
 
 if __name__ == "__main__":
@@ -118,10 +111,18 @@ if __name__ == "__main__":
     root_node = Node(gameBoard=root_gb)
     
     current_node = root_node
-    for i in range(10000000):
+    for i in range(10000000*1000):
+        
         if(len(current_node.untried_actions) == 0):
-            current_node = current_node.select()  
-        else:
+            isFinished,winner = current_node.gb.isTerminal()
+            if(isFinished):
+                current_node.backprop(winner)
+                current_node.gb.printBoard()
+                current_node = root_node
+            else:
+                current_node = current_node.select()  
+            
+        else:    
             current_node = current_node.expand()
 
             winner = current_node.simulate()
@@ -130,7 +131,11 @@ if __name__ == "__main__":
             current_node.gb.printBoard()
             
             current_node = root_node
+            print(i)
     
+    print("Process finished --- %s seconds ---" % (time.time() - start_time))
+
+
     def check_childs(node,state,the_node):
         for child in node.childs:
             if(child.state == state):
@@ -156,29 +161,19 @@ if __name__ == "__main__":
         symbol = "O"
         gb.move(symbol,best_move(gb.board,symbol))
         gb.printBoard()
-        
-
-            
-            
-            #check tempnnode childs
-           # check all childs of tempnode childs
-           # check all childs of that
-
-            
 
 
+"""
 
-        #find state in tree
-        #search childs, get one with max visits 
-        #TOO EEZZZ
+--------
+|      |
+|XOXOXO|
+|XOXOXO|
+|XOXOXO|
+|XOXOXO|
+|XOXOXO|
+|XOXOXO|
+--------
 
-            
-            
-            
-        
-        
-       
-    
-    #state, parent, symbol
 
-#9:58
+"""
