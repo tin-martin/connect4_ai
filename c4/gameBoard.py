209077729abd9970ae38faade4fb6482acf77df5
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2022-01-02 23:03:23 -0500

    sdf

--- c4/gameBoard.py
+++ c4/gameBoard.py
@@ -5,109 +5,78 @@ class gameBoard:
 		self.rows = 6
 		self.columns  =  7
 		self.board = board
-		
 	def getBoard(self):
 		return self.board
 	def printBoard(self):
-		for i in range(self.columns+2):
+		for i in range(self.rows+2):
 			print("-",end="")
 		print("")
-		for i in range(self.rows):
+		for i in range(len(self.board[0])):
 			print("|",end="")
-			for j in range(self.columns):
+			for j in range(len(self.board)):
 				print(self.board[j][i], end="")
 			print("|")
-		for i in range(self.columns+2):
+		for i in range(self.rows+2):
 			print("-",end="")
 		print("")
 		
 	def setBoard(self, new_board):
 		self.board = new_board
-
-	def move(self, symbol,col):
+#_________________
+	def move(self, symbol,row):
 		
 		board = deepcopy(self.getBoard())
 
-		if(board[col][0] != " "):
+		if(board[row][0] != " "):
 			raise ValueError('A very specific bad thing happened.')
 
-		for i in range(len(board[col])):
-			if (board[col][i] != " "):
-				board[col][i-1] = symbol
+		for i in range(len(board[row])):
+			if (board[row][i] != " "):
+				board[row][i-1] = symbol
 				break
-			if(i == len(board[col])-1):
-				board[col][i] = symbol			
+			if(i == len(board[row])-1):
+				board[row][i] = symbol			
 		return board
-
+#__________________
 	def check_straight(self,board,x,y):
 		symbol = board[x][y]
-		try:
-			if(board[x][y] == symbol and board[x][y+1] == symbol and board[x][y+2] == symbol and board[x][y+3] == symbol):
-				return True
-		except:
-			pass
+		if(board[x][y] == symbol and board[x][y+1] == symbol and board[x][y+2] == symbol and board[x][y+3] == symbol):
+			return True
 		return False
 	def check_diagonal(self,board,x,y):
 		symbol = board[x][y]
-		
-		try:
-			if(board[x][y] == symbol and board[x+1][y+1] == symbol and board[x+2][y+2] == symbol and board[x+3][y+3] == symbol):
-				return True
-		except:
-			pass
+		if(board[x][y] == symbol and board[x+1][y+1] == symbol and board[x+2][y+2] == symbol and board[x+3][y+3] == symbol):
+			return True
 		return False
-
-	def check_straight2(self,board,x,y):
-		symbol = board[x][y]
-		try:
-			if(board[x][y] == symbol and board[x+1][y] == symbol and board[x+2][y] == symbol and board[x+3][y] == symbol):
-				return True
-		except:
-			pass
-		return False
-	def check_diagonal2(self,board,x,y):
-		symbol = board[x][y]
-		try:
-			if(board[x][y] == symbol and board[x-1][y-1] == symbol and board[x-2][y-2] == symbol and board[x-3][y-3] == symbol):
-				return True
-		except:
-			pass
-		return False
-
 	def isTerminal(self):
-		board_tbc = self.board
+		board_tbc = deepcopy(np.array(self.board))
 		isTie = True
+		for z in range(4):
+			for x in range(len(board_tbc.tolist())):
+				for y in range(len(board_tbc.tolist()[0])):
 
-		for x in range(self.columns):
-			for y in range(self.rows):
-				
-				if(board_tbc[x][y] == ' '):	
-					isTie = False
-				if(self.check_diagonal(board_tbc,x,y) or self.check_straight(board_tbc,x,y) or self.check_diagonal2(board_tbc,x,y) or self.check_straight2(board_tbc,x,y)):
-					return True, board_tbc[x][y]
-				
-			
-			
+					if board_tbc[x][y] != ' ':	
 						
-		if(isTie):
-			return True, "TIE"
-			
-
+						try:
+							if(self.check_diagonal(board_tbc.tolist(),x,y) or self.check_straight(board_tbc.tolist(),x,y)):
+								return True, board_tbc[x][y]
+						except:
+							pass
+					else:
+						isTie = False
+							
+			if(isTie):
+				return True, "TIE"
+				
+			board_tbc = np.rot90(board_tbc,axes=(1,0))	
 		return False, " "
 
 	def get_legal_actions(self):
 		legal_actions = []
-		for i in range(self.columns):
+		for i in range(self.rows):
 			if(self.board[i][0] == " "):
 				legal_actions.append(i)
 		return legal_actions
-"""
 
 
 
-if __name__ == "__main__":
-	state = [[" "," "," "," "," ","X"],[" "," ","X"," ","X","X"],[" "," "," ","X"," "," "],[" "," ","X","X"," "," "],[" "," ","X","X"," "," "],[" "," "," "," "," "," "],[" "," "," "," "," "," "]]
-	gb = gameBoard(state)
-	print(gb.isTerminal())
-	print(gb.printBoard())
-"""
